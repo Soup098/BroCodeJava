b2eb8b237fa4806bcc40c1bea15e8720f608f038
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2025-07-11 20:00:47 -0600

    update
    practicing Java

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args){
+        //This is a comment in Java
+
+        /*
+        This
+        is a
+        multi
+        line
+        comment
+         */
+        System.out.println("hello world");
+        System.out.print("CHEEESSE\n");
+        System.out.println("Buy some cheese guy");
+        System.out.println();
+    }
+}
