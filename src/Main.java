dddda7f741c45f446dc718ffb217d959a8a2938f
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2025-07-14 20:41:17 -0600

    Update
    Madlib class created

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+        Madlib madlib = new Madlib();
 
-        System.out.print("Enter name: ");
-        String name = scanner.nextLine();
-
-        System.out.print("Enter age: ");
-        int age = scanner.nextInt();
-
-        System.out.print("Enter GPA: ");
-        double gpa = scanner.nextDouble();
-
-        System.out.print("Are you a student? (true/false): ");
-        boolean isStudent = scanner.nextBoolean();
-
-        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
-        System.out.println("Also, your gpa is: " + gpa);
-        if(isStudent){
-            System.out.println("Your are a student");
-        }else{
-            System.out.println("you are not a student");
-        }
-
-
-
-        scanner.close();//close the scanner after use
-
+        madlib.runMadlib();
 
     }
 }
