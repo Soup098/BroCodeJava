980cc51374a029d338494e243c93b3ebe69affd8
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2025-07-19 20:42:14 -0600

    update
    practice

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,26 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args){
-        CompoundInterest calculator = new CompoundInterest();
 
-        calculator.runInterestCalculator();
+        boolean isStudent = false;
+        boolean isSenior = false;
+        double price = 10;
+
+        if(isStudent){
+            if(isSenior){
+                System.out.println("You get a senior discount of 20%, and a student discount of 10%");
+                price *= 0.7;
+            }else{
+                System.out.println("You get a student discount of 10%");
+                price *= 0.9;
+            }
+        }else{
+            if(isSenior){
+                System.out.println("Your get a senior discount of 20%");
+                price *= 0.8;
+            }
+        }
+
+        System.out.printf("The price of the ticket is $%.2f", price);
     }
 }
