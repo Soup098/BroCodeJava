8faed3652ceed623e0fcc8e30a53fc540a117bfa
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2025-08-20 22:31:30 -0600

    practice

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void main(String[] args)
-    {
-        SimpleCalculator.runSimpleCalculator();
+    public static void main(String[] args){
+        double temp = 31;
+
+        if(temp <= 30 && temp >= 0){
+            System.out.println("The weather is good");
+        }else if(temp > 30 && temp < 35){
+            System.out.println("the weather is big");
+        }
     }
 }
