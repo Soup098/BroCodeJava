c1356df03c0f5795fd0ac88b9c2e70f7b9dbc8da
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2025-08-21 21:17:50 -0600

    practice

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args){
-        double temp = 31;
+        double temp = 100;
+        boolean isSunny = true;
 
-        if(temp <= 30 && temp >= 0){
+
+        if(temp <= 90 && temp >= 70){
             System.out.println("The weather is good");
-        }else if(temp > 30 && temp < 35){
-            System.out.println("the weather is big");
+        }else if(temp > 90 && temp < 110 && !isSunny){
+            System.out.println("the weather is hot and shitty");
         }
     }
 }
