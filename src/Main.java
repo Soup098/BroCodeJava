67beb20b280310b9c89be1dddbf9ee7b0979bc4e
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2025-08-24 05:23:59 -0600

    practice

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,16 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void main(String[] args){
-        NumberGuessingGame.runNumberGuessingGame();
+    public static void main(String[] args) throws InterruptedException {
+        //for loops
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Countdown start? ");
+        int start = scanner.nextInt();
+
+        for(int i  = start; i > 0; i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
     }
 }
