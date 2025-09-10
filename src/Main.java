a56b6acb11f0073436a59f0aacdbe1785a605d6d
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2025-09-10 12:48:26 -0600

    practice

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,18 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void main(String[] args){
+    static void singBirthday(String name, int age){
+        System.out.printf("Happy birthday %s \n", name);
+        System.out.printf("You are %d years old \n", age);
+    };
+    static double square(double num){
+        return num * num;
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        int rows;
-        int columns;
-        char symbol;
-        System.out.print("How many rows?:  ");
-        rows = scanner.nextInt();
-        System.out.print("How many columns?: ");
-        columns = scanner.nextInt();
-        System.out.print("what symbol?: ");
-        symbol = scanner.next().charAt(0);
 
-        for (int i = 1; i < rows + 1; i++){
-            System.out.print("Row " + i + ": ");
-            for (int j = 1; j < columns  + 1; j++){
-                System.out.print(symbol);
-            }
-            System.out.println();
-        }
-
-        scanner.close();
+    public static void main(String[] args) {
+        double number = 14;
+        System.out.println(square(number));
     }
 }
+
