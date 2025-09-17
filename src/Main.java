2f0e967e554b8b679961f19255c9741541715af7
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2025-09-17 06:35:01 -0600

    practice

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,22 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-public class Main
-{
-    static void singBirthday(String name, int age){
-        System.out.printf("Happy birthday %s \n", name);
-        System.out.printf("You are %d years old \n", age);
-    };
-    static double square(double num){
-        return num * num;
-    }
-    static double cube(double num){
-        return num * num * num;
-    }
-    static String getFullName(String firstName, String lastName){
-        return firstName + " " + lastName;
-    }
+// public method - accessible from anywhere
+// private method - accessible only within its class
+// static method - belongs to the class, not an instance (can be called without creating an object)
+// void method - does not return anything (just performs method action)
 
+
+public class Main {
     public static void main(String[] args) {
 
-        String first = "Alex";
-        String last = "Surprenant";
+        int x = 1;//LOCAL
 
-        System.out.println(getFullName(first, last));
+        System.out.println(x);
+
+
+
+
     }
 }
 
