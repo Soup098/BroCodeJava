c142fae49a2f32313b0326e8a1759dec4325e6e3
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2025-09-20 21:26:15 -0600

    practice

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Scanner;
 
 // public method - accessible from anywhere
@@ -10,7 +11,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        DiceRoller.runDiceRoller();
+        //arrays a collection of values of the same data type
+
+        String[] fruit = {"orange", "apple", "banana", "mango"};
+
+        Arrays.sort(fruit);
+        Arrays.fill(fruit, "Avacado");
+
+        for(String fruits : fruit){
+            System.out.print(fruits + " ");
+        }
     }
 }
 
